665a9f33fb52c2da701a189433bcbe663fca7336
Samantha Tucker <sammiegirl04@gmail.com>
2017-02-15 19:03:16 -0600

    initial formatting

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
-import cgi
 import jinja2
 import os
 from google.appengine.ext import db
 
 # set up jinja
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -31,12 +16,35 @@ class Handler(webapp2.RequestHandler):
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
+
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Blog(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(Handler):
+    def render_front(self, title="", body="", error=""):
+        blogs = db.GqlQuery("SELECT * FROM Blog"
+                            "ORDER BY created DESC")
+        self.render("front.html", title=title, body=body, error=error, blogs=blogs)
+
     def get(self):
-        self.response.write("Build a Blog")
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            a = Blog(title = title, body = body)
+            a.put()
+            self.redirect("/")
+        else:
+            error = "we need both a title and a body!"
+            self.render_front(title, body, error)
 
 app = webapp2.WSGIApplication([
     ('/', Handler)
